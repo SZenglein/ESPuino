5eb19a0bc19ae22c7ee9d2df2817728532ab66ed
Sascha <sascha>
2023-12-29 17:46:13 +0100

    update revision

--- src/revision.h
+++ src/revision.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #include "gitrevision.h"
-constexpr const char softwareRevision[] = "Software-revision: 20231217-1-szenglein";
+constexpr const char softwareRevision[] = "Software-revision: 20231228-2-szenglein";
